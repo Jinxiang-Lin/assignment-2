182c1302eab80f3126a0148f2e8d22ac5916849e
Jinxiang-Lin <78502763+Jinxiang-Lin@users.noreply.github.com>
2021-10-02 10:28:16 -0400

    myEach

---
+++ methods.js
@@ -0,0 +1,44 @@
+const arr = [1, 2];
+
+Array.prototype.myEach = function (callbackFn) {
+  for (let i = 0; i < this.length; i++) {
+    if (this[i] === undefined) continue;
+    callbackFn(this[i], i, this);
+  }
+};
+
+// arr.myEach((x, i, arr) => console.log(x, i, arr));
+// arr.forEach((x, i, arr) => console.log(x, i, arr));
+
+// MAP //
+Array.prototype.myMap = function () {};
+
+// FILTER //
+Array.prototype.myFilter = function () {};
+
+// SOME //
+Array.prototype.mySome = function () {};
+
+// EVERY //
+Array.prototype.myEvery = function () {};
+
+// REDUCE //
+Array.prototype.myReduce = function () {};
+
+// INCLUDES //
+Array.prototype.myIncludes = function () {};
+
+// INDEXOF //
+Array.prototype.myIndexOf = function () {};
+
+// PUSH //
+Array.prototype.myPush = function () {};
+
+// LASTINDEXOF //
+Array.prototype.myLastIndexOf = function () {};
+
+// KEYS //
+Object.grabKeys = function () {};
+
+// VALUES //
+Object.grabValues = function () {};
